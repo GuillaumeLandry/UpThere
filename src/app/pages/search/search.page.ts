a8080f70120966ff88846dce9b5d6dd6c82cfa71
Guillaume <guillaume@lepointdevente.com>
2020-10-04 16:45:29 -0400

    logos and sponsorship added

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -8,61 +8,55 @@ import { ProbeService } from 'src/app/services/probe.service';
 })
 export class SearchPage implements OnInit {
 
-  probes: any; /*
+  probes=[
     {
       img: "",
       name: "Himawari-9",
       country: "Japan",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: ""
     },    
     {
       img: "",
       name: "NOAA-18",
       country: "USA",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: "er geg aewgea rg aewrg"
     },
     {
       img: "",
       name: "Anik F1",
       country: "Canada",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: "sefwaefawff"
     },
     {
       img: "",
       name: "Anik F1",
       country: "Canada",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: "wea fawefa werfaerfaerfea rf"
     },
     {
       img: "",
       name: "Anik F1",
       country: "Canada",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: "aer aer aerf aergaerg aer"
     },
     {
       img: "",
       name: "Anik F1",
       country: "Canada",
-      category: "Himawari-9",
+      purpose: "Himawari-9",
       description: "gearg earg earg erg erg arg"
     },
-  ]*/
+  ]
   constructor(
     private probeService: ProbeService
   ) { }
 
   ngOnInit() {
-    const arr = [];
-    this.probes = this.probeService.getProbesInfo();
-    for (let i of this.probes) {
-      arr.push(i);
-    }
-    console.log(arr);
   }
 
   onProbeClick() {
