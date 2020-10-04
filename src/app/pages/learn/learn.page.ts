cc5d7d04d1a832e0a3f07189cf82ddc89f85a52f
Guillaume <guillaume@lepointdevente.com>
2020-10-04 12:50:10 -0400

    image content added and search/learn progress

--- src/app/pages/learn/learn.page.ts
+++ src/app/pages/learn/learn.page.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnPage implements OnInit {
 
+  selectedTab: string;
+
   constructor() { }
 
   ngOnInit() {}
+
+  segmentChanged(tab: any) {
+    this.selectedTab = tab;
+    console.log(this.selectedTab)
+    console.log(tab)
+  }
 
 }
